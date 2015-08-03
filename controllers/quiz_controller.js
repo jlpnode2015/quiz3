9e231d8fcfac0252f5e53299b5e4b3530c8f93cc
jlpnode2015 <jlpnode2015@gmail.com>
2015-08-03 17:30:55 +0200

    Proyecto Quiz
    Modificación Mod.8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,28 +1,115 @@
 var models = require('../models/models.js');
-//GET /quizes
-exports.index = function(req, res) {  
-  models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes});
-    })
+
+// Autoload :id
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else { next( new Error('No existe quizId = ' + quizId ))}
+    }
+  ).catch(function(error){ next(error); });
 };
 
-// GET /quizes/:id
+//GET /quizes
+exports.index = function(req, res) {
+	if(req.query.search) {    
+   		var search  = '%'+ req.query.search.replace(/ /g, '%') + '%';
+
+		models.Quiz.findAll({where: ["pregunta like ?", search], order: 'pregunta ASC'}).then(
+			function(quizes) {
+				res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+      			}
+    		).catch(function(error){next(error);});
+
+	} else {
+		models.Quiz.findAll().then(
+			function(quizes) {
+				res.render('quizes/index', { quizes: quizes, errors: []});
+			}
+		).catch(function(error){next(error);})
+	}
+};
+
+//GET /quizes
 exports.show = function(req, res) {
-	models.Quiz.find(req,params,quizId).then(function(quiz){  
-	res.render('quizes/show', { quiz: quiz});
-     })
-};       
+        res.render('quizes/show', { quiz: req.quiz, errors: [] });
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+    var quiz = req.quiz;
+    res.render('quizes/edit', {quiz: quiz, errors: []});
+};
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-	models.Quiz.find(req,params,quizId),then(function(quiz){  
-		if (req.query.respuesta === quiz.respuesta) {
-   			res.render('quizes/answer', 
-				{ quiz: quiz, respuesta: "Correcto"});
-		}else{
-			res.render('quizes/answer', 
-				{ quiz: quiz, respuesta: "Incorrecto"}); 
-      		}
-  	})
+  var resultado = 'Incorrecto';
+    if (req.query.respuesta === req.quiz.respuesta){
+        resultado = 'Correcto';
+    }
+    res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
+// Get /author
+exports.author = function(req, res) {
+	res.render('author', {author: 'Fernando Castaño Torres', errors: []});
+}
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(  //crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta", tematica: "Tematica"}
+  );
+  res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+// GET /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build(req.body.quiz);
+  quiz.validate().then(
+	function(err) {
+		if(err) {
+			res.render('quizes/new', {quiz: quiz, errors: err.errors});
+		} else {
+			//guarda en DB los campos pregunta y respuesta de quiz
+  			quiz.save({fields: ["pregunta", "respuesta", "tematica"]}).then(function(){
+    				res.redirect('/quizes');
+  			});
+
+		}
+	}
+  );
+};
+
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tematica = req.body.quiz.tematica;
+	
+	req.quiz.
+	validate()
+	.then(
+        function(err) {
+                if(err) {
+                        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+                } else {
+                        //guarda en DB los campos pregunta y respuesta de quiz
+                        req.quiz.save({fields: ["pregunta", "respuesta", "tematica"]}).then(function(){
+                                res.redirect('/quizes');
+                        });
+
+                }
+        }
+	);
+
+}
+
+// Delete 
+exports.destroy = function(req, res) {
+	console.log('Se va a borrar la pregunta ' + req.quiz.pregunta);
+	req.quiz.destroy().then(function() {
+		res.redirect('/quizes');
+	}).catch(function(error) {next(error);});
+}
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,24 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. revisar */
+/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
-// Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizID
 
-//Autor 
-router.get('/author', function(req, res) {
-res.render('author', { title: 'Autor' });
-});
+router.get('/author',			quizController.author);
 
+router.get('/quizes',				quizController.index);
+router.get('/quizes/:quizId(\\d+)',		quizController.show);
+router.get('/quizes/:quizId(\\d+)/edit',        quizController.edit)
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
+
+router.get('/quizes/new', 			quizController.new);
+router.post('/quizes/create', 			quizController.create);
+router.put('/quizes/:quizId(\\d+)',		quizController.update);
+router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
 
 module.exports = router;
